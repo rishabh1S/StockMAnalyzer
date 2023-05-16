b25a5cd1ee25b7766922b28482082e3e59d4d444
rishabh1S <rishabh15112001@gmail.com>
2023-05-16 17:27:28 +0530

    init commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
+import firebase from 'firebase/compat/app';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  user$: Observable<firebase.User>;
+
+  constructor(private afAuth: AngularFireAuth) {
+    this.user$ = afAuth.authState;
+  }
+
+  register(
+    email: string,
+    password: string
+  ): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.createUserWithEmailAndPassword(email, password);
+  }
+
+  login(
+    email: string,
+    password: string
+  ): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout(): Promise<void> {
+    return this.afAuth.signOut();
+  }
+}
